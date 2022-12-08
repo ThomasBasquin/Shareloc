9774c1d185b7fe55cc7f6e72a4f30213c5340e95
[Roméo] [PROBST] <rprobst@unistra.fr>
2022-12-08 14:49:47 +0100

    Installation dépendances / Navigation

---
+++ Mobile/ShareLoc/src/screens/Authentification.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+const Authentification = ({navigation}) => {
+    return (
+        <View>
+            <Text>Bite</Text>
+        </View>
+    );
+}
+export default Authentification;
