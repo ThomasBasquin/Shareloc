31e2c4db265a02979bf7d87a9304b3f9dcbdee54
[Roméo] [PROBST] <rprobst@unistra.fr>
2022-12-09 15:14:57 +0100

    ajout composant BoxGrise

--- Mobile/ShareLoc/src/screens/Accueil.js
+++ Mobile/ShareLoc/src/screens/Accueil.js
@@ -2,12 +2,17 @@ import React from "react";
 import { Text, View } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import Title from "../components/Title";
+import BoxGrise from "../components/BoxGrise"
+
+
 
 const Accueil = ({ navigation }) => {
   return (
     <View style={{ flex: 1, backgroundColor: "white" }}>
       <Title title="Accueil" />
-      <Ionicons name="md-checkmark-circle" size={32} color="green" />
+      <BoxGrise>
+        <Text>Mes points</Text>
+      </BoxGrise>
     </View>
   );
 };
