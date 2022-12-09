c2d9507a60b20f7c4f74c2db4f6aea0ee74a6557
[Roméo] [PROBST] <rprobst@unistra.fr>
2022-12-09 16:00:47 +0100

    Début partie mes services page accueil

--- Mobile/ShareLoc/src/screens/Accueil.js
+++ Mobile/ShareLoc/src/screens/Accueil.js
@@ -1,38 +1,84 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, ScrollView } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import Title from "../components/Title";
-import BoxGrise from "../components/BoxGrise"
-import { FontAwesome } from '@expo/vector-icons';
-import {COLOR} from "../constantes/Color"
-
+import BoxGrise from "../components/BoxGrise";
+import Box from "../components/Box";
+import { FontAwesome } from "@expo/vector-icons";
+import { COLOR } from "../constantes/Color";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const Accueil = ({ navigation }) => {
   return (
-    <View style={{ flex: 1, backgroundColor: "white" }}>
-      <Title title="Accueil" />
-      <BoxGrise>
-        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-        <Text style={styles.points}>Mes points</Text>
-        <Text style={styles.pointsScore}>76 <FontAwesome name="star" size={24} color={COLOR.jaune} /></Text>
-        
+    <ScrollView style={{ backgroundColor: COLOR.blanc }}>
+      <View style={{ flex: 1, backgroundColor: COLOR.blanc }}>
+        <Title title="Accueil" />
+        <MesPoints />
+        <MesServices />
+      </View>
+    </ScrollView>
+  );
+};
 
-        </View>
-      </BoxGrise>
+const MesPoints = () => {
+  return (
+    <BoxGrise>
+      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+        <Text style={styles.points}>Mes points</Text>
+        <Text style={styles.pointsScore}>
+          76 <FontAwesome name="star" size={24} color={COLOR.jaune} />
+        </Text>
+      </View>
+    </BoxGrise>
+  );
+};
+
+const MesServices = () => {
+  return (
+    <View>
+      <Text style={styles.titreMesServices}>Mes services en cours :</Text>
+      <Box>
+        <BoxGrise>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <View style={{alignItems: "center", flexDirection: "row"}}>
+              <MaterialIcons
+                name="cleaning-services"
+                color={COLOR.bleuFonce}
+                size={18}
+              />
+              <Text style={styles.infoService}>Passez l'aspirateur</Text>
+            </View>
+          </View>
+        </BoxGrise>
+      </Box>
     </View>
   );
 };
 const styles = StyleSheet.create({
   points: {
     fontSize: 20,
-    fontWeight: '600'
+    fontWeight: "600",
   },
   pointsScore: {
     fontSize: 20,
-    fontWeight: '600',
-    
+    fontWeight: "600",
   },
-
+  titreMesServices: {
+    fontSize: 20,
+    fontWeight: "600",
+    marginLeft: 12,
+    marginTop: 10,
+  },
+  infoService: {
+    fontSize: 15,
+    marginLeft: 10,
+  },
 });
 
 export default Accueil;
