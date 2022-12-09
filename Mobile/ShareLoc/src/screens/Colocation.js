973d37f3aad35afc8ba8aaf6ced937e9bcba65d8
Thomas <thomas.basquin2@gmail.com>
2022-12-09 11:15:50 +0100

    Add BoxService component

--- Mobile/ShareLoc/src/screens/Colocation.js
+++ Mobile/ShareLoc/src/screens/Colocation.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import Ionicons from '@expo/vector-icons/Ionicons';
-import Title from '../components/Title'
+import React from "react";
+import { Text, View } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import Title from "../components/Title";
 
-
-const Colocation = ({navigation}) => {
-    return (
-        
-        <View style={{ flex: 1, backgroundColor: 'white' }}>
-            <Title title="Colocation" />
-            <Ionicons name='md-checkmark-circle' size={32} color='green' />
-        </View>
-    )
-}
+const Colocation = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      <Title title="Colocation" />
+      <Ionicons name="md-checkmark-circle" size={32} color="green" />
+    </View>
+  );
+};
 
 export default Colocation;
