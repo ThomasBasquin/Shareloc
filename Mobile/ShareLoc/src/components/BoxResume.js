845f92452d7d324b5f74aae752afee37b790a4bb
[Roméo] [PROBST] <rprobst@unistra.fr>
2022-12-09 11:19:00 +0100

    Début design page Colocation

---
+++ Mobile/ShareLoc/src/components/BoxResume.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { COLOR } from "../constantes/Color";
+
+/**
+ * 
+ * @param {*} children composants enfants 
+ * @param  other props
+ * @returns 
+ */
+export default function BoxResume({
+    children,
+    ...other
+}){
+
+    return (
+        <View style={styles.view} {...other}>
+            {children}
+        </View>
+    )
+}
+
+const styles=new StyleSheet.create({
+    view:{
+        margin:"5%",
+        padding:10,
+        borderColor: COLOR.gris,
+        backgroundColor:COLOR.bleuFonce,
+        borderWidth:1,
+        borderRadius:15,
+        width:100,
+        elevation: 3,
+    }
+})
--- Mobile/ShareLoc/src/screens/Colocation.js
+++ Mobile/ShareLoc/src/screens/Colocation.js
@@ -1,17 +1,45 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import Title from '../components/Title'
+import Box from '../components/Box'
+import BoxResume from '../components/BoxResume'
+import {COLOR} from "../constantes/Color"
 
 
 const Colocation = ({navigation}) => {
     return (
         
         <View style={{ flex: 1, backgroundColor: 'white' }}>
-            <Title title="Colocation" />
+            <Title title="Ma colocation" />
+            <Box>
+                <View>
+                    <Text style={styles.text}>Résumé</Text>
+                    <BoxResume>
+                        <Text style={styles.titreBoxResume}>Participants</Text>
+                    </BoxResume>
+                </View>
+            </Box>
             <Ionicons name='md-checkmark-circle' size={32} color='green' />
         </View>
     )
 }
-
+const styles = new StyleSheet.create({
+    text: {
+      fontSize: 18,
+      fontWeight: "600",
+      color: COLOR.bleuFonce,
+      width:"100%", 
+      textAlign: 'center'
+    },
+    titreBoxResume: {
+        fontSize: 14,
+        fontWeight: "500",
+        color: COLOR.jaune,
+        width:"100%", 
+        textAlign: 'center'
+      },
+    
+    
+  });
 export default Colocation;
