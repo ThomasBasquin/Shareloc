3d7e05f473ed83ddf4e28a23154dac64ba3b2659
[Roméo] [PROBST] <rprobst@unistra.fr>
2022-12-09 15:23:18 +0100

    Mes points page Accueil

--- Mobile/ShareLoc/src/screens/Accueil.js
+++ Mobile/ShareLoc/src/screens/Accueil.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import Title from "../components/Title";
 import BoxGrise from "../components/BoxGrise"
-
+import { FontAwesome } from '@expo/vector-icons';
+import {COLOR} from "../constantes/Color"
 
 
 const Accueil = ({ navigation }) => {
@@ -11,10 +12,27 @@ const Accueil = ({ navigation }) => {
     <View style={{ flex: 1, backgroundColor: "white" }}>
       <Title title="Accueil" />
       <BoxGrise>
-        <Text>Mes points</Text>
+        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+        <Text style={styles.points}>Mes points</Text>
+        <Text style={styles.pointsScore}>76 <FontAwesome name="star" size={24} color={COLOR.jaune} /></Text>
+        
+
+        </View>
       </BoxGrise>
     </View>
   );
 };
+const styles = StyleSheet.create({
+  points: {
+    fontSize: 20,
+    fontWeight: '600'
+  },
+  pointsScore: {
+    fontSize: 20,
+    fontWeight: '600',
+    
+  },
+
+});
 
 export default Accueil;
