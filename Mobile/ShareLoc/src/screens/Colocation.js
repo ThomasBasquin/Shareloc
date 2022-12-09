bdb26566cdf58d05754cc0d29abbdf0a236f7629
[Roméo] [PROBST] <rprobst@unistra.fr>
2022-12-09 13:57:37 +0100

    Ajout d'un scrollView

--- Mobile/ShareLoc/src/screens/Colocation.js
+++ Mobile/ShareLoc/src/screens/Colocation.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, ScrollView } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import Title from "../components/Title";
 import Box from "../components/Box";
@@ -9,16 +9,19 @@ import { FontAwesome } from "@expo/vector-icons";
 
 const Colocation = ({ navigation }) => {
   return (
+    <ScrollView>
     <View style={{ flex: 1, backgroundColor: "white" }}>
       <Title title="Ma colocation" />
       <Resume />
       <Participants />
     </View>
+    </ScrollView>
   );
 };
 
 const Resume = () => {
   return (
+    
     <Box>
       <View>
         <Text style={styles.text}>Résumé</Text>
