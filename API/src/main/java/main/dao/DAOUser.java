33d1275de0998b4a634967f34a062e4e1eccb68b
hugos <schellinger@c-koya.tech>
2022-12-10 19:39:03 +0100

    refont api

---
+++ API/src/main/java/main/dao/DAOUser.java
@@ -0,0 +1,74 @@
+package main.dao;
+
+import main.model.AchievedService;
+import main.model.User;
+
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class DAOUser extends DAOAbstractFacade<User> {
+
+	public DAOUser() {
+		super(User.class);
+	}
+
+	public User getUser(Long userId) {
+		TypedQuery<User> query = getEntityManager().createQuery("SELECT u FROM User u WHERE u.id = :userId", User.class)
+				.setParameter("userId", userId);
+		try {
+			return query.getSingleResult();
+		} catch (NoResultException e) {
+			return null;
+		}
+	}
+
+	public User login(String login, String password) {
+		TypedQuery<User> query = getEntityManager()
+				.createQuery("SELECT u FROM User u WHERE u.login = :login AND u.password = :password", User.class)
+				.setParameter("login", login).setParameter("password", password);
+		try {
+			return query.getSingleResult();
+		} catch (NoResultException e) {
+			return null;
+		}
+	}
+
+	public User getUserByLogin(String login) {
+		TypedQuery<User> query = getEntityManager()
+				.createQuery("SELECT u FROM User u WHERE u.login = :login", User.class).setParameter("login", login);
+		try {
+			return query.getSingleResult();
+		} catch (NoResultException e) {
+			return null;
+		}
+	}
+
+	public List<User> getCollocationUsers(Long collocationId) {
+		TypedQuery<User> query = getEntityManager()
+				.createQuery("SELECT u FROM User u JOIN u.collocations c WHERE c.id = :collocationId", User.class)
+				.setParameter("collocationId", collocationId);
+		return query.getResultList();
+	}
+	
+	public int getUserPoints(Long userId, Long collocationId) {
+		TypedQuery<Long> query1 = getEntityManager().createQuery(
+				"SELECT SUM(s.points) FROM User u JOIN u.collocations c JOIN c.services s JOIN s.achievedServices aService JOIN aService.author author "
+				+ "WHERE u.id = :userId AND c.id = :collocationId AND aService.validated = :validated AND author.id = :authorId", Long.class)
+				.setParameter("collocationId", collocationId)
+				.setParameter("userId", userId)
+				.setParameter("authorId", userId);
+		int gainPoints = query1.getSingleResult() != null ? query1.getSingleResult().intValue() : 0;
+		System.out.println(gainPoints);
+		
+		TypedQuery<Long> query2 = getEntityManager().createQuery(
+				"SELECT SUM(s.points) * COUNT(aService.beneficiaries) FROM User u JOIN u.collocations c JOIN c.services s JOIN s.achievedServices aService JOIN aService.author author "
+				+ "WHERE u.id = :userId AND c.id = :collocationId AND aService.validated = :validated AND author.id != :authorId", Long.class)
+				.setParameter("collocationId", collocationId)
+				.setParameter("userId", userId)
+				.setParameter("authorId", userId);
+		int lostPoints = query2.getSingleResult() != null ? query2.getSingleResult().intValue() : 0;
+		System.out.println(gainPoints);
+		return gainPoints - lostPoints;
+	}
+}
---
+++ API/src/main/java/main/path/ServiceInvitation.java
@@ -0,0 +1,69 @@
+package main.path;
+
+import main.controller.ControllerUser;
+import main.model.User;
+import main.security.SigninNeeded;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.SecurityContext;
+import java.util.List;
+
+
+//@SigninNeeded
+//@Path("/invitations")
+//public class ServiceInvitation {
+//
+//	/**
+//	 * Renvoie les invitations en cours de l'utilisateur
+//	 * @param security Contexte de securit�
+//	 * @return Les invitations en cours
+//	 */
+//	@GET
+//	@Path("/")
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public List<> getUserPendingInvitations(@Context SecurityContext security) {
+//		User user = ControllerUser.getUser(Long.parseLong(security.getUserPrincipal().getName()));
+//		return ControllerInvitation.getUserPendingInvitations(user);
+//	}
+//
+//	/**
+//	 * Accepte l'invitation d'id pass� en parametre
+//	 * @param security Contexte de securit�
+//	 * @param id Id de l'invitation a accepter
+//	 * @return Response
+//	 */
+//	@GET
+//	@Path("/{id}/accept")
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public Response acceptInvitation(@Context SecurityContext security, @PathParam("id") Long id) {
+//		User user = ControllerUser.getUser(Long.parseLong(security.getUserPrincipal().getName()));
+//		if (ControllerInvitation.acceptInvitation(id, user))
+//			return Response.ok().build();
+//		else
+//			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+//	}
+//
+//	/**
+//	 * Refuse l'invitation d'id pass� en parametre
+//	 * @param security Contexte de securit�
+//	 * @param id Id de l'invitation a accepter
+//	 * @return Response
+//	 */
+//	@GET
+//	@Path("/{id}/deny")
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public Response denyInvitation(@Context SecurityContext security, @PathParam("id") Long id) {
+//		User user = ControllerUser.getUser(Long.parseLong(security.getUserPrincipal().getName()));
+//		if (ControllerInvitation.denyInvitation(id, user))
+//			return Response.ok().build();
+//		else
+//			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+//	}
+//}
