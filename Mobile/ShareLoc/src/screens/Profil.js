168043243d5917aaa31ecd3611639f9ecc3887ba
Thomas <thomas.basquin2@gmail.com>
2022-12-15 14:42:01 +0100

    Création de deux composants pour info modifiable ou non

--- Mobile/ShareLoc/src/screens/Profil.js
+++ Mobile/ShareLoc/src/screens/Profil.js
@@ -19,17 +19,7 @@ import { COLOR } from "../constantes/Color";
 
 const Profil = ({ navigation }) => {
   const [modalVisibility, setModalVisibility] = useState(false);
-  const [Nom, setNom] = useState("Basquin");
-  const [Prenom, setPrenom] = useState("Thomas");
-  const [Email, setEmail] = useState("thomas.basquin2@gmail.com");
-  const [number, setNumber] = useState("06 12 34 56 78");
-  const [Password, setPassword] = useState("PetitCoquin");
-  const [showPassword, setShowPassword] = useState(false);
   const [editable, setEditable] = useState(false);
-
-  const handleShowPassword = () => {
-    setShowPassword(!showPassword);
-  };
 
   const handleEditable = () => {
     setEditable(!editable);
@@ -64,91 +54,7 @@ const Profil = ({ navigation }) => {
               <Text style={{ color: "white" }}>Quitter ma colocation</Text>
             </ButtonComponent>
 
-            <View
-              style={{
-                bottom: 70,
-                elevation: 12,
-                borderRadius: 20,
-                padding: 15,
-                paddingRight: 15,
-                backgroundColor: COLOR.blanc,
-              }}
-            >
-              <Text style={[styles.label]}>Nom :</Text>
-              <TextInput
-                style={styles.input}
-                value={Nom}
-                onChangeText={setNom}
-                editable={editable ? true : false}
-              />
-              <Text style={styles.label}>Prénom :</Text>
-              <TextInput
-                style={styles.input}
-                value={Prenom}
-                onChangeText={setPrenom}
-                editable={editable ? true : false}
-              />
-              <Text style={styles.label}>Email :</Text>
-              <TextInput
-                style={styles.input}
-                value={Email}
-                onChangeText={setEmail}
-                editable={editable ? true : false}
-              />
-              <Text style={styles.label}>Numéro de portable :</Text>
-              <TextInput
-                style={styles.input}
-                value={number}
-                onChangeText={setNumber}
-                editable={editable ? true : false}
-              />
-              <Text style={styles.label}>Mot de passe :</Text>
-              <View
-                style={{
-                  flexDirection: "row",
-                  alignItems: "center",
-                  borderWidth: 1,
-                  width: 275,
-                  height: 40,
-                  marginLeft: 0,
-                  marginTop: 7,
-                  marginBottom: 15,
-                  borderColor: COLOR.grisFonce,
-                  borderRadius: 15,
-                }}
-              >
-                <TextInput
-                  style={[
-                    styles.input,
-                    {
-                      width: 240,
-                      borderWidth: 0,
-                      margin: 0,
-                      padding: 0,
-                      marginBottom: 7,
-                      marginLeft: 10,
-                    },
-                  ]}
-                  value={Password}
-                  onChangeText={setPassword}
-                  secureTextEntry={!showPassword ? true : false}
-                  textContentType="oneTimeCode"
-                  editable={editable ? true : false}
-                ></TextInput>
-                <TouchableOpacity
-                  onPressIn={() => {
-                    handleShowPassword();
-                  }}
-                >
-                  <Ionicons
-                    name={!showPassword ? "eye-off" : "eye"}
-                    size={24}
-                    color={COLOR.grisFonce}
-                    style={{ right: 12, marginBottom: 0 }}
-                  />
-                </TouchableOpacity>
-              </View>
-            </View>
+            {!editable ? <ShowInfo /> : <EditInfo />}
             <ButtonComponent
               style={{ width: 230, marginLeft: 75, marginTop: -30 }}
               primary
@@ -178,6 +84,195 @@ const Profil = ({ navigation }) => {
         </ModalGeneral>
       </ScrollView>
     </SafeAreaView>
+  );
+};
+
+const ShowInfo = () => {
+  const [Nom, setNom] = useState("Basquin");
+  const [Prenom, setPrenom] = useState("Thomas");
+  const [Email, setEmail] = useState("thomas.basquin2@gmail.com");
+  const [number, setNumber] = useState("06 12 34 56 78");
+  const [Password, setPassword] = useState("PetitCoquin");
+
+  return (
+    <View>
+      <View
+        style={{
+          bottom: 70,
+          elevation: 12,
+          borderRadius: 20,
+          padding: 15,
+          paddingRight: 15,
+          backgroundColor: COLOR.blanc,
+        }}
+      >
+        <Text style={[styles.label]}>Nom :</Text>
+        <TextInput
+          style={styles.input}
+          value={Nom}
+          onChangeText={setNom}
+          editable={false}
+        />
+        <Text style={styles.label}>Prénom :</Text>
+        <TextInput
+          style={styles.input}
+          value={Prenom}
+          onChangeText={setPrenom}
+          editable={false}
+        />
+        <Text style={styles.label}>Email :</Text>
+        <TextInput
+          style={styles.input}
+          value={Email}
+          onChangeText={setEmail}
+          editable={false}
+        />
+        <Text style={styles.label}>Numéro de portable :</Text>
+        <TextInput
+          style={styles.input}
+          value={number}
+          onChangeText={setNumber}
+          editable={false}
+        />
+        <Text style={styles.label}>Mot de passe :</Text>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            borderWidth: 1,
+            width: 275,
+            height: 40,
+            marginLeft: 0,
+            marginTop: 7,
+            marginBottom: 15,
+            borderColor: COLOR.grisFonce,
+            borderRadius: 15,
+          }}
+        >
+          <TextInput
+            style={[
+              styles.input,
+              {
+                width: 240,
+                borderWidth: 0,
+                margin: 0,
+                padding: 0,
+                marginBottom: 7,
+                marginLeft: 10,
+              },
+            ]}
+            value={Password}
+            onChangeText={setPassword}
+            secureTextEntry={true}
+            textContentType="oneTimeCode"
+            editable={false}
+          ></TextInput>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const EditInfo = () => {
+  const [Nom, setNom] = useState("Basquin");
+  const [Prenom, setPrenom] = useState("Thomas");
+  const [Email, setEmail] = useState("thomas.basquin2@gmail.com");
+  const [number, setNumber] = useState("06 12 34 56 78");
+  const [Password, setPassword] = useState("PetitCoquin");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <View>
+      <View
+        style={{
+          bottom: 70,
+          elevation: 12,
+          borderRadius: 20,
+          padding: 15,
+          paddingRight: 15,
+          backgroundColor: COLOR.blanc,
+        }}
+      >
+        <Text style={[styles.label]}>Nom :</Text>
+        <TextInput
+          style={styles.input}
+          value={Nom}
+          onChangeText={setNom}
+          editable={true}
+        />
+        <Text style={styles.label}>Prénom :</Text>
+        <TextInput
+          style={styles.input}
+          value={Prenom}
+          onChangeText={setPrenom}
+          editable={true}
+        />
+        <Text style={styles.label}>Email :</Text>
+        <TextInput
+          style={styles.input}
+          value={Email}
+          onChangeText={setEmail}
+          editable={true}
+        />
+        <Text style={styles.label}>Numéro de portable :</Text>
+        <TextInput
+          style={styles.input}
+          value={number}
+          onChangeText={setNumber}
+          editable={true}
+        />
+        <Text style={styles.label}>Mot de passe :</Text>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            borderWidth: 1,
+            width: 275,
+            height: 40,
+            marginLeft: 0,
+            marginTop: 7,
+            marginBottom: 15,
+            borderColor: COLOR.grisFonce,
+            borderRadius: 15,
+          }}
+        >
+          <TextInput
+            style={[
+              styles.input,
+              {
+                width: 240,
+                borderWidth: 0,
+                margin: 0,
+                padding: 0,
+                marginBottom: 7,
+                marginLeft: 10,
+              },
+            ]}
+            value={Password}
+            onChangeText={setPassword}
+            secureTextEntry={!showPassword ? true : false}
+            textContentType="oneTimeCode"
+            editable={true}
+          ></TextInput>
+          <TouchableOpacity
+            onPressIn={() => {
+              handleShowPassword();
+            }}
+          >
+            <Ionicons
+              name={!showPassword ? "eye-off" : "eye"}
+              size={24}
+              color={COLOR.grisFonce}
+              style={{ right: 12, marginBottom: 0 }}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
   );
 };
 
