0c916c081a04ee4e5816b221cfaf4b5f368f5a42
[Roméo] [PROBST] <rprobst@unistra.fr>
2022-12-15 14:00:55 +0100

    Design Page Colocation + structure Colocation

--- Mobile/ShareLoc/src/screens/Colocation.js
+++ Mobile/ShareLoc/src/screens/Colocation.js
@@ -1,38 +1,57 @@
 import React from "react";
-import { Text, View, StyleSheet, ScrollView, TouchableOpacity } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import Title from "../components/Title";
 import Box from "../components/Box";
 import BoxResume from "../components/BoxResume";
 import { COLOR } from "../constantes/Color";
 import { FontAwesome } from "@expo/vector-icons";
-import { Octicons } from '@expo/vector-icons';
-
+import { Octicons } from "@expo/vector-icons";
+import ServiceComponent from "../components/ServiceComponent";
+import ButtonComponent from "../components/ButtonComponent"
 
 const Colocation = ({ navigation }) => {
   return (
-    <ScrollView>
-    <View style={{ flex: 1, backgroundColor: "white" }}>
-    <View style={{ justifyContent: "space-between", flexDirection: "row", backgroundColor: COLOR.blanc}}>
-    <Title title="Ma colocation" />
-      <TouchableOpacity
-                onPressIn={() => {
-                  navigation.navigate("Messagerie")
-                }}
-              >
-      <Octicons name="feed-discussion" size={35} color={COLOR.bleuFonce} style={{margin:25}}/></TouchableOpacity>
+    <ScrollView style={{ backgroundColor: COLOR.blanc, marginBottom: 50 }}>
+      <View style={{ flex: 1, backgroundColor: "white" }}>
+        <View
+          style={{
+            justifyContent: "space-between",
+            flexDirection: "row",
+            backgroundColor: COLOR.blanc,
+          }}
+        >
+          <Title title="Ma colocation" />
+          <TouchableOpacity
+            onPressIn={() => {
+              navigation.navigate("Messagerie");
+            }}
+          >
+            <Octicons
+              name="feed-discussion"
+              size={35}
+              color={COLOR.bleuFonce}
+              style={{ margin: 25 }}
+            />
+          </TouchableOpacity>
+        </View>
+
+        <Resume />
+        <Participants />
+        <ServicesEnCours navigation={navigation} />
       </View>
-      
-      <Resume />
-      <Participants />
-    </View>
     </ScrollView>
   );
 };
 
 const Resume = () => {
   return (
-    
     <Box>
       <View>
         <Text style={styles.text}>Résumé</Text>
@@ -112,39 +131,61 @@ const Resume = () => {
 const Participants = () => {
   return (
     <View style={styles.participants}>
-      <Text style={styles.titreParticipants}>Participants</Text>
+      <Text style={styles.titreParticipants}>Participants :</Text>
       <Box>
-      <View style={styles.itemParticipants}>
-        <Text style={styles.prenom}>
-          Roméo <Text style={styles.role}>(créateur)</Text>
-        </Text>
-        <Text style={styles.nbPoints}>76 <Text style={{color : COLOR.jaune}}>pts</Text></Text>
-      </View>
-      <View style={styles.itemParticipants}>
-        <Text style={styles.prenom}>
-          Thomas <Text style={styles.role}>(participant)</Text>
-        </Text>
-        <Text style={styles.nbPoints}>50 <Text style={{color : COLOR.jaune}}>pts</Text></Text>
-      </View>
-      <View style={styles.itemParticipants}>
-        <Text style={styles.prenom}>
-          Hugo <Text style={styles.role}>(participant)</Text>
-        </Text>
-        <Text style={styles.nbPoints}>35 <Text style={{color : COLOR.jaune}}>pts</Text></Text>
-      </View>
-      <View style={styles.itemParticipants}>
-        <Text style={styles.prenom}>
-          Gaytan <Text style={styles.role}>(participant)</Text>
-        </Text>
-        <Text style={styles.nbPoints}>14 <Text style={{color : COLOR.jaune}}>pts</Text></Text>
-      </View>
-      <View style={styles.itemParticipants}>
-        <Text style={styles.prenom}>
-          Lucas <Text style={styles.role}>(participant)</Text>
-        </Text>
-        <Text style={styles.nbPoints}>0 <Text style={{color : COLOR.jaune}}>pts</Text></Text>
-      </View>
+        <ItemParticipants prenom="Roméo" role="créateur" points={76} />
+        <ItemParticipants prenom="Thomas" role="participant" points={50} />
+        <ItemParticipants prenom="Hugo" role="participant" points={35} />
+        <ItemParticipants prenom="Gaytan" role="participant" points={14} />
+        <ItemParticipants prenom="Lucas" role="participant" points={0} />
       </Box>
+    </View>
+  );
+};
+const ItemParticipants = ({ prenom, role, points }) => {
+  return (
+    <View style={styles.itemParticipants}>
+      <Text style={styles.prenom}>
+        {prenom} <Text style={styles.role}>({role})</Text>
+      </Text>
+      <Text style={styles.nbPoints}>
+        {points} <Text style={{ color: COLOR.jaune }}>pts</Text>
+      </Text>
+    </View>
+  );
+};
+
+const ServicesEnCours = ({ navigation }) => {
+  return (
+    <View style={styles.participants}>
+      <Text style={styles.titreParticipants}>Services en cours :</Text>
+      <ServiceComponent
+        navigation={navigation}
+        date="18/02/2022"
+        by="Hugo"
+        pour="Roméo"
+        label="Passez l'aspirateur"
+        score={10}
+      />
+      <ServiceComponent
+        navigation={navigation}
+        date="18/02/2022"
+        by="Hugo"
+        pour="Roméo"
+        label="Passez l'aspirateur"
+        score={10}
+      />
+      <ServiceComponent
+        navigation={navigation}
+        date="18/02/2022"
+        by="Hugo"
+        pour="Roméo"
+        label="Passez l'aspirateur"
+        score={10}
+      />
+      
+      <ButtonComponent primary onPress={()=> {navigation.navigate("Services")}}>Voir tout</ButtonComponent>
+    
     </View>
   );
 };
@@ -166,14 +207,14 @@ const styles = new StyleSheet.create({
     marginBottom: 5,
   },
   participants: {
-    margin: "5%",
-    
+    margin: "2%",
   },
   titreParticipants: {
     fontSize: 25,
     fontWeight: "800",
     color: COLOR.bleuFonce,
     marginBottom: 10,
+    marginLeft: 10,
   },
   prenom: {
     fontSize: 19,
@@ -189,13 +230,17 @@ const styles = new StyleSheet.create({
     padding: 10,
     flexDirection: "row",
     justifyContent: "space-between",
-    borderBottomWidth : 1,
-    borderStyle: 'dashed',
-    borderColor : COLOR.gris
+    borderBottomWidth: 1,
+    borderStyle: "dashed",
+    borderColor: COLOR.gris,
   },
   nbPoints: {
-    fontSize:16,
-
+    fontSize: 16,
+  },
+  voirTout : {
+    fontSize: 18,
+    fontWeight: "600",
+    margin :10
   }
 });
 
