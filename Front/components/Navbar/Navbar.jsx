eb5571ff0e6b74c7f6dbdf75505a353176150772
[Roméo] [PROBST] <rprobst@unistra.fr>
2023-01-12 10:01:11 +0100

    Navbar

--- Front/components/Navbar/Navbar.jsx
+++ Front/components/Navbar/Navbar.jsx
@@ -8,20 +8,20 @@ export default function Navbar() {
     <nav className="navbar">
       <img className="logo" src="../../src/assets/logo.png" />
       <div className="link">
-        <img src="../../src/assets/home-sharp.svg" />
+        {/* <img src="../../src/assets/home-sharp.svg" /> */}
         <Link to="/welcome" className="itemNavbar">
           Accueil
         </Link>
-        <img src="../../src/assets/people-sharp.svg" />
+        {/* <img src="../../src/assets/people-sharp.svg" /> */}
         <Link to="/colocation" className="itemNavbar">
           Colocation
         </Link>
-        <img src="../../src/assets/networking-svgrepo-com.svg" />
+        {/* <img src="../../src/assets/networking-svgrepo-com.svg" /> */}
 
         <Link to="/service" className="itemNavbar">
           Services
         </Link>
-        <img src="../../src/assets/profile-user-svgrepo-com.svg" />
+        {/* <img src="../../src/assets/profile-user-svgrepo-com.svg" /> */}
         <Link to="/compte" className="itemNavbar">
           Compte
         </Link>
