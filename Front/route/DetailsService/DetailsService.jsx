5529a36888d9373f17d6b600964fcbd69374167a
[Roméo] [PROBST] <rprobst@unistra.fr>
2023-01-12 17:25:43 +0100

    cheplu

--- Front/route/DetailsService/DetailsService.jsx
+++ Front/route/DetailsService/DetailsService.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Form, Link, Outlet, useLoaderData, useParams } from "react-router-dom";
 import Box from "../../components/Box/Box";
-
+import Navbar from "../../components/Navbar/Navbar";
 import ButtonComponent from "../../components/ButtonComponent/ButtonComponent";
 
 export default function Authentification() {
   return (
     <div className="">
+        <Navbar />
         <p>cc</p>
     </div>
   )
