b9f1d536077abbc8f6cd8febaf11a67f927f29bd
[Roméo] [PROBST] <rprobst@unistra.fr>
2023-01-13 11:12:07 +0100

    test svg marche pas c nul

--- Front/route/Colocation/Colocation.jsx
+++ Front/route/Colocation/Colocation.jsx
@@ -9,9 +9,11 @@ export default function Colocation() {
   return (
     <>
       <Navbar />
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <div style={{ display: "flex", justifyContent: "space-between"}} >
         <Title title="Colocation" />
         <Resume />
+        
+
       </div>
     </>
   );
@@ -21,11 +23,11 @@ const Resume = () => {
   return (
     <div className="resume">
       <Box style={{ width: "60%", marginRight: 700, marginTop: 40 }}>
-        <div className="boxResume">
+        <div className="text-white">
           <div
             style={{ display: "flex", justifyContent: "center", margin: 10 }}
           >
-            <h1 style={{fontWeight: 500, fontSize : 45}}>Résumé</h1>
+            <h1 style={{fontWeight: 500, fontSize : 38}}>Résumé</h1>
           </div>
           <div className="itemResume">
             <Box style={{ backgroundColor: COLOR.bleuFonce, width: 200, display : 'flex', justifyContent: 'center', margin : 20}}>
@@ -42,6 +44,7 @@ const Resume = () => {
               </div>
 
             </Box>
+            
           </div>
           <div className="itemResume">
           <Box style={{ backgroundColor: COLOR.bleuFonce, width: 200, display : 'flex', justifyContent: 'center', margin : 20}}>
