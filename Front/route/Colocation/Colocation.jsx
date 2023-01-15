57ff9d500791e41dc1d0ac19aa717e885cd1bf53
[Roméo] [PROBST] <rprobst@unistra.fr>
2023-01-15 19:15:01 +0100

    design classement

--- Front/route/Colocation/Colocation.jsx
+++ Front/route/Colocation/Colocation.jsx
@@ -10,15 +10,14 @@ export default function Colocation() {
   return (
     <>
       <Navbar />
-      <div style={{ display: "flex", justifyContent: "space-between"}} >
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
         <Title title="Colocation" />
         <Resume />
       </div>
-      <div style={{ display: "flex", justifyContent: "space-between"}} >
-      <Classement />
-      <ServicesColoc />
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <Classement />
+        <ServicesColoc />
       </div>
-      
     </>
   );
 }
@@ -31,39 +30,90 @@ const Resume = () => {
           <div
             style={{ display: "flex", justifyContent: "center", margin: 10 }}
           >
-            <h1 style={{fontWeight: 500, fontSize : 38}}>Résumé</h1>
+            <h1 style={{ fontWeight: 500, fontSize: 38 }}>Résumé</h1>
           </div>
           <div className="itemResume">
-            <Box style={{ backgroundColor: COLOR.bleuFonce, width: 200, display : 'flex', justifyContent: 'center', margin : 20}}>
-              <div style={{display : 'flex', flexDirection : 'column', justifyContent: 'center'}}>
-              <p className="titreItemResume">Participants</p>
-              <p className="valeurItemResume">5</p>
+            <Box
+              style={{
+                backgroundColor: COLOR.bleuFonce,
+                width: 200,
+                display: "flex",
+                justifyContent: "center",
+                margin: 20,
+              }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                }}
+              >
+                <p className="titreItemResume">Participants</p>
+                <p className="valeurItemResume">5</p>
               </div>
-
             </Box>
-            <Box style={{ backgroundColor: COLOR.bleuFonce, width: 200, display : 'flex', justifyContent: 'center', margin : 20}}>
-              <div style={{display : 'flex', flexDirection : 'column', justifyContent: 'center'}}>
-              <p className="titreItemResume">Services</p>
-              <p className="valeurItemResume">12</p>
+            <Box
+              style={{
+                backgroundColor: COLOR.bleuFonce,
+                width: 200,
+                display: "flex",
+                justifyContent: "center",
+                margin: 20,
+              }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                }}
+              >
+                <p className="titreItemResume">Services</p>
+                <p className="valeurItemResume">12</p>
               </div>
-
             </Box>
-            
           </div>
           <div className="itemResume">
-          <Box style={{ backgroundColor: COLOR.bleuFonce, width: 200, display : 'flex', justifyContent: 'center', margin : 20}}>
-              <div style={{display : 'flex', flexDirection : 'column', justifyContent: 'center'}}>
-              <p className="titreItemResume">Points</p>
-              <p className="valeurItemResume">667</p>
+            <Box
+              style={{
+                backgroundColor: COLOR.bleuFonce,
+                width: 200,
+                display: "flex",
+                justifyContent: "center",
+                margin: 20,
+              }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                }}
+              >
+                <p className="titreItemResume">Points</p>
+                <p className="valeurItemResume">667</p>
               </div>
-
             </Box>
-            <Box style={{ backgroundColor: COLOR.bleuFonce, width: 200, display : 'flex', justifyContent: 'center', margin : 20}}>
-              <div style={{display : 'flex', flexDirection : 'column', justifyContent: 'center'}}>
-              <p className="titreItemResume">Top coloc</p>
-              <p className="valeurItemResume">Hugo</p>
+            <Box
+              style={{
+                backgroundColor: COLOR.bleuFonce,
+                width: 200,
+                display: "flex",
+                justifyContent: "center",
+                margin: 20,
+              }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                }}
+              >
+                <p className="titreItemResume">Top coloc</p>
+                <p className="valeurItemResume">Hugo</p>
               </div>
-
             </Box>
           </div>
         </div>
@@ -74,35 +124,31 @@ const Resume = () => {
 
 const Classement = () => {
   return (
-    <BoxGrise style={{width: '50%'}}>
+    <BoxGrise style={{ width: "50%", backgroundColor : COLOR.bleuFonce, padding:20 }}>
       <h1 className="titleClassement">Classement :</h1>
-      <ItemClassement prenom="Roméo" role="créateur" points={76}/>
+      <ItemClassement prenom="Roméo" role="créateur" points={76} />
+      <ItemClassement prenom="Thomas" role="participant" points={50} />
+      <ItemClassement prenom="Hugo" role="participant" points={35} />
+      <ItemClassement prenom="Gaytan" role="participant" points={14} />
+      <ItemClassement prenom="Lucas" role="participant" points={0} />
     </BoxGrise>
-  )
-}
+  );
+};
 const ServicesColoc = () => {
-  return (
-    <div>
-      cc
-    </div>
-  )
-}
+  return <div>cc</div>;
+};
 
 const ItemClassement = ({ prenom, role, points }) => {
   return (
     <div className="itemParticipants">
-      <div style={{display : 'flex', flexDirection : 'row'}}>
-      <p className="prenom">
-        {prenom} 
-      </p>
-      <p className="role">({role})</p>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <p className="prenom">{prenom}</p>
+        <p className="role">({role})</p>
       </div>
-      <div style={{display : 'flex', flexDirection : 'row'}}>
-      <p className="nbPoints">
-        {points} 
-      </p>
-      <p style={{ color: COLOR.jaune }}>pts</p>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <p className="nbPoints">{points}</p>
+        <p style={{ color: COLOR.jaune, fontSize:35 }}>pts</p>
       </div>
     </div>
-  )
-}
+  );
+};
