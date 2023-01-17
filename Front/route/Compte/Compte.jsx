0f1a8b8c7a04b12eb40b0a309b3bcd7503a123cc
ThomasBasquin <thomas.basquin2@gmail.com>
2023-01-17 23:24:58 +0100

    web profil adapted

--- Front/route/Compte/Compte.jsx
+++ Front/route/Compte/Compte.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useContext } from "react";
+import React, { useState, useContext } from "react";
 
 import { Link } from "react-router-dom";
 import Title from "../../components/Title/Title";
@@ -6,7 +6,8 @@ import { AuthContext } from "../../context/AuthContext";
 import "./Compte.css";
 
 export default function Profil() {
-  const {logout} = useContext(AuthContext);
+  const { logout } = useContext(AuthContext);
+  const { editable, setEditable } = useState(false);
 
   return (
     <>
@@ -21,14 +22,14 @@ export default function Profil() {
             </Link>
           </div>
           <div className="button-container">
-              <button onClick={logout} className="button" id="button-disconnect">
-                Déconnexion
-              </button>
+            <button onClick={logout} className="button" id="button-disconnect">
+              Déconnexion
+            </button>
           </div>
         </div>
       </div>
-      <ShowInfo />
-      <EditInfo />
+
+      {!editable ? <ShowInfo /> : <EditInfo />}
     </>
   );
 }
@@ -39,58 +40,72 @@ const ShowInfo = () => {
   const [Email, setEmail] = useState("thomas.basquin2@gmail.com");
   const [number, setNumber] = useState("06 12 34 56 78");
   const [Password, setPassword] = useState("PetitCoquin");
-  const [showPassword, setShowPassword] = useState(false);
-
-  const handleShowPassword = () => {
-    setShowPassword(!showPassword);
-  };
 
   return (
     <div>
       <div
         className="info-container"
-        style={{ padding: 15, backgroundColor: "white" }}
+        style={{ padding: 25, backgroundColor: "white" }}
       >
-        <label className="label">Nom :</label>
-        <input
-          className="input"
-          type="text"
-          value={Nom}
-          onChange={(e) => setNom(e.target.value)}
-          readOnly={true}
-        />
-        <label className="label">Prénom :</label>
-        <input
-          className="input"
-          type="text"
-          value={Prenom}
-          onChange={(e) => setPrenom(e.target.value)}
-          readOnly={true}
-        />
-        <label className="label">Email :</label>
-        <input
-          className="input"
-          type="email"
-          value={Email}
-          onChange={(e) => setEmail(e.target.value)}
-          readOnly={true}
-        />
-        <label className="label">Numéro de portable :</label>
-        <input
-          className="input"
-          type="tel"
-          value={number}
-          onChange={(e) => setNumber(e.target.value)}
-          readOnly={true}
-        />
-        <label className="label">Mot de passe :</label>
-        <div className="password-container">
+        <div className="name-div info-div">
+          <div id="name-div">
+            <label className="label">Nom :</label>
+            <input
+              className="input"
+              type="text"
+              value={Nom}
+              onChange={(e) => setNom(e.target.value)}
+              readOnly={true}
+            />
+          </div>
+          <div id="first-name-div">
+            <label className="label">Prénom :</label>
+            <input
+              className="input"
+              type="text"
+              value={Prenom}
+              onChange={(e) => setPrenom(e.target.value)}
+              readOnly={true}
+            />
+          </div>
+        </div>
+        <div className="alone-div info-div">
+          <label className="label" id="alone-label">
+            Email :
+          </label>
+          <input
+            className="input"
+            type="email"
+            value={Email}
+            onChange={(e) => setEmail(e.target.value)}
+            readOnly={true}
+            id="alone-input"
+          />
+        </div>
+        <div className="alone-div info-div">
+          <label className="label" id="alone-label">
+            Numéro de portable :
+          </label>
+          <input
+            className="input"
+            type="tel"
+            value={number}
+            onChange={(e) => setNumber(e.target.value)}
+            readOnly={true}
+            id="alone-input"
+          />
+        </div>
+        <div className="alone-div info-div">
+          <label className="label" id="alone-label">
+            Mot de passe :
+          </label>
           <input
             className="input"
             type="password"
             value={Password}
             onChange={(e) => setPassword(e.target.value)}
             readOnly={true}
+            id="alone-input"
           />
         </div>
       </div>
