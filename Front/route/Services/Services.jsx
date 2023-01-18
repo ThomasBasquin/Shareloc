c0da39db8ed4c303a82f947db9f4166b45ec1963
ThomasBasquin <thomas.basquin2@gmail.com>
2023-01-18 23:00:49 +0100

    Trois colonnes pour services

--- Front/route/Services/Services.jsx
+++ Front/route/Services/Services.jsx
@@ -1,18 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Navbar from "../../components/Navbar/Navbar"
-import Title from "../../components/Title/Title"
-
+import Navbar from "../../components/Navbar/Navbar";
+import Title from "../../components/Title/Title";
+import ServiceComponent from "../../components/ServiceComponent/ServiceComponent.jsx";
+import "./Services.css";
 
 export default function Service() {
+  const [date, setDate] = useState("25/02/2023");
+  const [by, setBy] = useState("Thomas");
+  const [pour, setPour] = useState("Roméo");
+  const [label, setLabel] = useState("Réparer la porte");
+  const [score, setScore] = useState(5);
+
   return (
     <>
-    <Title title="Services" />
-    <div className="text-white" >
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-</svg>
-</div>
+      <Title title="Services" />
+      <div className="service-div">
+        <ServicesAsked
+          date={date}
+          by={by}
+          pour={pour}
+          label={label}
+          score={score}
+        />
+        <ServicesToDo
+          date={date}
+          by={by}
+          pour={pour}
+          label={label}
+          score={score}
+        />
+        <ServicesDone
+          date={date}
+          by={by}
+          pour={pour}
+          label={label}
+          score={score}
+        />
+      </div>
     </>
   );
 }
+
+const ServicesAsked = ({ date, by, pour, label, score }) => {
+  return (
+    <>
+      <div className="service-container">
+        <h2 className="service-title">Mes services demandés :</h2>
+        <ServiceComponent
+          className="service-component"
+          date={date}
+          by={by}
+          pour={pour}
+          label={label}
+          score={score}
+        />
+      </div>
+    </>
+  );
+};
+
+const ServicesToDo = ({ date, by, pour, label, score }) => {
+  return (
+    <>
+      <div className="service-container">
+        <h2 className="service-title">Mes services :</h2>
+        <div className="service-container">
+          <ServiceComponent
+            className="service-component"
+            date={date}
+            by={by}
+            pour={pour}
+            label={label}
+            score={score}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+const ServicesDone = ({ date, by, pour, label, score }) => {
+  return (
+    <>
+      <div className="service-container">
+        <h2 className="service-title">Mes services effectués :</h2>
+        <div className="service-container">
+          <ServiceComponent
+            className="service-component"
+            date={date}
+            by={by}
+            pour={pour}
+            label={label}
+            score={score}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
