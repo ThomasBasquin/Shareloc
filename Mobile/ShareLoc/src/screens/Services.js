60a5992d9d9b05196f30cb91b2193fd5cbeec792
hugos <schellinger@c-koya.tech>
2023-01-19 14:28:05 +0100

    Création de services mobile

--- Mobile/ShareLoc/src/screens/Services.js
+++ Mobile/ShareLoc/src/screens/Services.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext,useEffect } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import {
   Text,
   View,
@@ -17,47 +17,70 @@ import { UserContext } from "../Context/UserContext";
 import useFetch from "../constantes/UseFetch";
 import URLS from "../constantes/Routes";
 import moment from "moment";
-import DropDownPicker from 'react-native-dropdown-picker';
+import DropDownPicker from "react-native-dropdown-picker";
 
 const Services = ({ navigation }) => {
   const [modalVisibility, setModalVisibility] = useState(false);
-  const [serviceCreated, setServiceCreated] = useState({title:"",performer:"",recipient:"",cost:0});
+
+  const [title, setTitle] = useState("");
+  const [performer, setPerformer] = useState(null);
+  const [recipient, setRecipient] = useState(null);
+  const [cost, setCost] = useState(0);
 
   const { user } = useContext(UserContext);
 
+  const [openPickerPerformer, setOpenPickerPerformer] = useState(false);
+  const [openPickerRecipient, setOpenPickerRecipient] = useState(false);
   const [recipientServices, setRecipientServices] = useState([]);
   const [performerServices, setPerformerServices] = useState([]);
   const [members, setMembers] = useState([]);
 
   useEffect(() => {
-    let promiseAll=[];
-    promiseAll.push(useFetch(URLS.getServicesRecipient.replace("{user}", user.id)));
-    promiseAll.push(useFetch(URLS.getServicesPerformer.replace("{user}", user.id)));
-    if(user.colocation){
-      useFetch(URLS.getCollocation.replace("{collocation}", user.colocation))
-  .then((c)=> setMembers(c.members))
-    }
-
-    Promise.all(promiseAll)
-    .then(([recipient,performer])=>{
-      setRecipientServices(recipient);
-      setPerformerServices(performer);
-    })
+    fetchServices();
   }, []);
 
-  console.log(members);
+  function fetchServices(){
+    let promiseAll = [];
+    promiseAll.push(
+      useFetch(URLS.getServicesRecipient.replace("{user}", user.id))
+    );
+    promiseAll.push(
+      useFetch(URLS.getServicesPerformer.replace("{user}", user.id))
+    );
+    if (user.colocation) {
+      useFetch(
+        URLS.getCollocation.replace("{collocation}", user.colocation)
+      ).then((c) => setMembers(c.members.map(m => ({label : m.firstname + " " + m.lastname,value:m.id}))));
+    }
+
+    Promise.all(promiseAll).then(([recipient, performer]) => {
+      setRecipientServices(recipient);
+      setPerformerServices(performer);
+      setTitle("");
+      setPerformer(null);
+      setRecipient(null);
+      setCost(0);
+      setModalVisibility(false);
+    });
+  }
 
 
   const annuler = () => {
     setModalVisibility(!modalVisibility);
-    setServiceCreated({title:"",performer:"",recipient:"",cost:0});
+    setTitle("");
+    setPerformer(null);
+    setRecipient(null);
+    setCost(0);
   };
 
   const valider = () => {
-    setModalVisibility(!modalVisibility);
-    setServiceCreated({title:"",performer:"",recipient:"",cost:0});
-    useFetch(URLS.createService,"POST",{})
+    useFetch(URLS.createService, "POST", {title,performer,recipient,cost,collocation:user.colocation})
+    .then(()=>{
+      fetchServices();
+    })
+    .catch(e => console.log(e))
   };
+
   return (
     <ScrollView style={{ backgroundColor: COLOR.blanc, marginBottom: 50 }}>
       <View
@@ -89,34 +112,65 @@ const Services = ({ navigation }) => {
           <Text>Créer un service</Text>
         </ButtonComponent>
       ) : null}
-      <ServiceBeneficiaire navigation={navigation} services={recipientServices} />
-      <ServiceActionnaire navigation={navigation} services={performerServices} />
-      <ServiceFinis navigation={navigation} services={[...recipientServices,...performerServices].filter(s => s.validatedAt!==null)} />
+      <ServiceBeneficiaire
+        navigation={navigation}
+        services={recipientServices}
+      />
+      <ServiceActionnaire
+        navigation={navigation}
+        services={performerServices}
+      />
+      <ServiceFinis
+        navigation={navigation}
+        services={[...recipientServices, ...performerServices].filter(
+          (s) => s.validatedAt !== null
+        )}
+      />
       <ModalGeneral visible={modalVisibility}>
         <Text style={styles.titreModal}>Créez un service</Text>
         <TextInput
           style={styles.input}
-          value={serviceCreated.title}
+          value={title}
           placeholder="Nom du service"
-          onChangeText={e => setServiceCreated({...serviceCreated,title:e})}
+          onChangeText={(e) =>
+            setTitle(e)
+          }
         />
-        <TextInput
-          style={styles.input}
-          value={serviceCreated.performer}
+        <DropDownPicker
+          mode="SIMPLE"
+          dropDownDirection="TOP"
+          open={openPickerPerformer}
+          value={performer}
+          items={members}
+          setOpen={setOpenPickerPerformer}
+          setValue={setPerformer}
+          setItems={setMembers}
+          theme="LIGHT"
           placeholder="Actionnaire"
-          onChangeText={e => setServiceCreated({...serviceCreated,performer:e})}
+          multiple={false}
+          style={styles.input}
+        />
+        <DropDownPicker
+          mode="SIMPLE"
+          dropDownDirection="BOTTOM"
+          open={openPickerRecipient}
+          setOpen={setOpenPickerRecipient}
+          value={recipient}
+          setValue={setRecipient}
+          items={members}
+          setItems={setMembers}
+          theme="LIGHT"
+          placeholder="Bénéficiare"
+          multiple={false}
+          style={styles.input}
         />
         <TextInput
           style={styles.input}
-          value={serviceCreated.recipient}
-          placeholder="Bénéficiaire"
-          onChangeText={e => setServiceCreated({...serviceCreated,recipient:e})}
-        />
-        <TextInput
-          style={styles.input}
-          value={serviceCreated.cost}
+          value={cost}
           placeholder="Nombre de points"
-          onChangeText={e => setServiceCreated({...serviceCreated,cost:parseInt(e)})}
+          onChangeText={(e) =>
+            setCost(parseInt(e))
+          }
           keyboardType="numeric"
         />
         <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
@@ -136,35 +190,42 @@ const ServiceActionnaire = ({ navigation, services }) => {
       <Text style={styles.titrePartie}>
         Mes services en tant que actionnaire :
       </Text>
-      { services.length ? services.map(s => (
-        <ServiceComponent
-        navigation={navigation}
-        date={moment(s.createdAt).format("LL")}
-        by={s.performer.firstname}
-        pour={s.recipient.firstname}
-        label={s.title}
-        score={s.cost}
-      />
-        )) : <Text>Vous n'avez aucun services en tant que actionnaire</Text> }
+      {services.length ? (
+        services.map((s) => (
+          <ServiceComponent
+            navigation={navigation}
+            date={moment(s.createdAt).format("LL")}
+            by={s.performer.firstname}
+            pour={s.recipient.firstname}
+            label={s.title}
+            score={s.cost}
+          />
+        ))
+      ) : (
+        <Text>Vous n'avez aucun services en tant que actionnaire</Text>
+      )}
     </View>
   );
 };
 
-const ServiceBeneficiaire = ({ navigation,services }) => {
-
+const ServiceBeneficiaire = ({ navigation, services }) => {
   return (
     <View style={{ flex: 1, backgroundColor: COLOR.blanc, margin: 10 }}>
       <Text style={styles.titrePartie}>Mes services en cours :</Text>
-      { services.length ? services.map(s => (
-        <ServiceComponent
-        navigation={navigation}
-        date={moment(s.createdAt).format("LL")}
-        by={s.performer.firstname}
-        pour={s.recipient.firstname}
-        label={s.title}
-        score={s.cost}
-      />
-        )) : <Text>Vous n'avez aucun services en tant que bénéficiaire</Text> }
+      {services.length ? (
+        services.map((s) => (
+          <ServiceComponent
+            navigation={navigation}
+            date={moment(s.createdAt).format("LL")}
+            by={s.performer.firstname}
+            pour={s.recipient.firstname}
+            label={s.title}
+            score={s.cost}
+          />
+        ))
+      ) : (
+        <Text>Vous n'avez aucun services en tant que bénéficiaire</Text>
+      )}
     </View>
   );
 };
@@ -173,17 +234,20 @@ const ServiceFinis = ({ navigation, services }) => {
   return (
     <View style={{ flex: 1, backgroundColor: COLOR.blanc, margin: 10 }}>
       <Text style={styles.titrePartie}>Mes services terminés :</Text>
-      { services.length ? services.map(s => (
-        <ServiceComponent
-        navigation={navigation}
-        date={moment(s.createdAt).format("LL")}
-        by={s.performer.firstname}
-        pour={s.recipient.firstname}
-        label={s.title}
-        score={s.cost}
-      />
-        )) : <Text>Vous n'avez aucun services terminés</Text> }
-
+      {services.length ? (
+        services.map((s) => (
+          <ServiceComponent
+            navigation={navigation}
+            date={moment(s.createdAt).format("LL")}
+            by={s.performer.firstname}
+            pour={s.recipient.firstname}
+            label={s.title}
+            score={s.cost}
+          />
+        ))
+      ) : (
+        <Text>Vous n'avez aucun services terminés</Text>
+      )}
     </View>
   );
 };
